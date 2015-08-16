b4476a72178b0a9d17af40536a8fdfed1e33d52c
Alison Monteiro <alisonmonteiro.10@gmail.com>
2015-08-16 17:56:00 -0300

    Add parameters in node file

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 var request = require('request'),
 	fs = require('fs'),
+	myHost = 'http://cssinliner.dev/',
 	myBody = null,
-	myHost = null,
+	myPath = null,
+	myOutput = 'output',
 	myPrefix = '- ';
 
-request('http://cssinliner.dev/examples/example.php', 
-	function(err, response, body) {
-		if(err) return console.log(err);
-
-		myBody = body;
-  		console.log(myPrefix + 'Its works');
-		
-		fs.writeFile('my-result.html', myBody, 
-			function(err) {
-			    if(err) {
-			        return console.log(err);
-			    }
-
-			    console.log(myPrefix + 'The file was saved!');
-			}
-		);	
+process.argv.forEach(
+	function(val, index, array) {
+		if(index == 2) myPath = val;
+		if(index == 3) myOutput = val;
 	}
 );
+
+if(myPath != null){
+	try {
+		stats = fs.lstatSync(myPath);
+	    if (stats.isDirectory()) {
+	        
+			request(myHost + myPath + '/' + myPath + '.php', 
+				function(err, response, body) {
+
+					if(err) console.log(err);
+
+					myBody = body;
+			  		console.log(myPrefix + 'Its works');
+					
+					fs.writeFile(myPath + '/' + myOutput + '.html', myBody, 
+						function(err) {
+						    if(err) {
+						        return console.log(err);
+						    }
+
+						    console.log(myPrefix + 'The file was saved!');
+						}
+					);	
+				}
+			);
+
+	    }
+	}catch (e){
+		console.log(e);
+	}		
+}
