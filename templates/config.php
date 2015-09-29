afc09eb1812dbf6f574b9ecb56b423008527bce3
Jiri Travnicek <jiri.travnicek@nubium.cz>
2015-09-29 02:28:17 +0200

    use variable instead of

--- templates/config.php
+++ templates/config.php
@@ -5,5 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 use Northys\CSSInliner\CSSInliner;
 
 $inliner = new CSSInliner;
-$inliner->addCSS(__DIR__ . '/' . $_GET['template'] . '/' . $_GET['template'] . '.css');
-echo $inliner->render(file_get_contents(__DIR__ . '/' . $_GET['template'] . '/' . $_GET['template'] . '.html'));
+$template = $_GET['template'];
+$inliner->addCSS(__DIR__ . '/' . $template . '/' . $template . '.css');
+echo $inliner->render(file_get_contents(__DIR__ . '/' . $template . '/' . $template . '.html'));
