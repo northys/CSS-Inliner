9491484fe9c9b0faecc3f0fa0214bbaa81b9c7b9
Jiri Travnicek <jiri.travnicek@nubium.cz>
2016-12-04 13:55:35 +0100

    psr-2

--- templates/config.php
+++ templates/config.php
@@ -1,10 +1,10 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 use Northys\CSSInliner\CSSInliner;
 
-$inliner = new CSSInliner;
+$inliner = new CSSInliner();
 $template = $_GET['template'];
-$inliner->addCSS(__DIR__ . '/' . $template . '/' . $template . '.css');
-echo $inliner->render(file_get_contents(__DIR__ . '/' . $template . '/' . $template . '.html'));
+$inliner->addCSS(__DIR__.'/'.$template.'/'.$template.'.css');
+echo $inliner->render(file_get_contents(__DIR__.'/'.$template.'/'.$template.'.html'));
--- tests/CSSInlinerTest.php
+++ tests/CSSInlinerTest.php
@@ -1,25 +1,23 @@
 <?php
 
-namespace Tests;
+namespace tests;
+
 use Northys\CSSInliner\CSSInliner;
 
 /**
- * Class CSSInlinerTest
- *
- * @package Tests
+ * Class CSSInlinerTest.
  */
 class CSSInlinerTest extends \PHPUnit_Framework_TestCase
 {
-
-	/**
-	 * Test Component
-	 */
-	public function testComponent()
-	{
-		$inliner = new CSSInliner;
-		$inliner->addCSS(__DIR__ . '/test.css');
-		$output = $inliner->render(file_get_contents('test.html'), true);
-		$expected = <<<EXPECTED
+    /**
+     * Test Component.
+     */
+    public function testComponent()
+    {
+        $inliner = new CSSInliner();
+        $inliner->addCSS(__DIR__.'/test.css');
+        $output = $inliner->render(file_get_contents('test.html'), true);
+        $expected = <<<'EXPECTED'
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html><body><h1 style="color: #27ae60; font-size: 200px; margin: 10px 50px 80px 30px;">Hello, world!</h1>
 <a href="http://google.com" class="google" style='color: #2c3e50;color: #c0392b; font-weight: 700; font-family: Verdana,"Open Sans"; font-size: 30px;'>Google</a>
@@ -27,7 +25,6 @@ class CSSInlinerTest extends \PHPUnit_Framework_TestCase
 <a href="http://Outlook.com" id="outlook" style="color: #2c3e50;color: #2980b9; position: absolute; top: 30px; left: 500px; padding: 50px;">Outlook</a></body></html>
 
 EXPECTED;
-		$this->assertSame($expected, $output);
-	}
-
+        $this->assertSame($expected, $output);
+    }
 }
